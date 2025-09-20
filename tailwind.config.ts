3af934cbbb6890da723bff98438d18f002b3fe31
star coder <starcoder8@gmail.com>
2025-09-20 12:24:18 -0700

    deploy again

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,8 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
